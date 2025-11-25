cc58da692ecbfa9856b9182b4b620e431070186c
jogaroca <jogarosales@gmail.com>
2025-11-24 18:48:15 -0800

    Update A_bd_dynamics.py

--- Phase_1_Bd_Dynamics_Demo/A_bd_dynamics.py
+++ Phase_1_Bd_Dynamics_Demo/A_bd_dynamics.py
@@ -1,36 +1,46 @@
-import sys
-sys.path.append('../Utility')  # para poder importar bd_chemostat
+# Phase_1_Bd_Dynamics_Demo/A_bd_dynamics.py
 
-import bd_chemostat
+import sys
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Añadir ../Utility al path de Python (para importar bd_chemostat)
+CURRENT_DIR = os.path.dirname(__file__)
+UTILITY_DIR = os.path.join(CURRENT_DIR, "..", "Utility")
+sys.path.append(UTILITY_DIR)
+
+import bd_chemostat as bd
+
 
 def main():
-    # Definir f y h (igual estilo que planar_drone)
-    f = bd_chemostat.F().f
-    h = bd_chemostat.H('h_BNW').h   # y = [B, N, W]
-    
-    t_sim, x_sim, u_sim, y_sim = bd_chemostat.simulate_bd(
-        f, h,
+    # Definir dinámica y medición
+    f = bd.F().f
+    h = bd.H("h_BNW").h  # y = [B, N, W]
+
+    # Simular
+    t_sim, x_sim, u_sim, y_sim = bd.simulate_bd(
+        f,
+        h,
         tsim_length=48.0,
         dt=0.01,
-        D=0.12
+        D=0.12,
     )
-    
-    # Separar salidas
+
     B = y_sim[:, 0]
     N = y_sim[:, 1]
     W = y_sim[:, 2]
-    
-    plt.figure()
-    plt.plot(t_sim, B, label='B (biomasa total)')
-    plt.plot(t_sim, N, label='N (nutriente)')
-    plt.plot(t_sim, W, label='W (waste)')
-    plt.xlabel('t [h]')
-    plt.legend()
-    plt.tight_layout()
+
+    # Graficar
+    fig, ax = plt.subplots()
+    ax.plot(t_sim, B, label="B (biomasa total)")
+    ax.plot(t_sim, N, label="N (nutriente)")
+    ax.plot(t_sim, W, label="W (waste)")
+    ax.set_xlabel("t [h]")
+    ax.legend()
+    fig.tight_layout()
     plt.show()
+
 
 if __name__ == "__main__":
     main()
